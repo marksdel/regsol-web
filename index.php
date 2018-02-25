4cf2b6c9738aad8ab506d3fc4a28ec0a9db1140b
marksdel <36573913+marksdel@users.noreply.github.com>
2018-02-25 23:00:29 +0000

    Move to php, include menu

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
     <meta property="og:title" content="RegSol">
 	<meta property="og:description" content="Regulatory and compliance solutions for European firms">
     
-    <link rel="icon" href="images/regsol_logo_black_yellow	.png">
+    <link rel="icon" href="images/regsol_logo_black_yellow.png">
 	
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Comfortaa">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/main.css">
 	<link rel="stylesheet" href="css/style.css">
 	
-    <script src="https://lndr.io/js/jquery/jquery.min.js"></script>
-    <script src="https://lndr.io/js/bootstrap.min.js"></script>
-    <script src="https://lndr.io/js/html5shiv.min.js"></script>
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/html5shiv.min.js"></script>
 
     
 </head>
@@ -36,53 +36,7 @@
 
 
     <!-- NAVBAR -->
-
-    <nav class="navbar navbar-default navbar-fixed-top header-background">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              
-
-			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#home"><span><img height="700px" src="images/regsol_logo_black_yellow.png"></span></a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-					<div class="dropdown nav navbar-left">
-						<li><a href="index.html">Home</a></li>
-					</div>
-					<div class="dropdown nav navbar-left">
-						<li><a href="training.html">Training</a></li>
-					</div>
-						
-					<div class="dropdown nav navbar-left">
-						<li><a href="consulting.html">Consulting</a></li>
-						<div class="dropdown-content">
-							<a href="consulting.html#AML">AML</a>
-							<a href="consulting.html#authorizations">Authorizations</a>		
-							<a href="consulting.html#GDPR">GDPR</a>							
-						</div>
-					</div>
-							
-                    
-					<div class="dropdown nav navbar-left">
-						<li><a href="about_us.html">About Us</a></li>
-						<div class="dropdown-content">
-							<a href="about_us.html#our_story">Our Story</a>
-							<a href="about_us.html#contact">Contact Us</a>		
-							<a href="about_us.html#privacy">Privacy</a>
-						</div>
-					</div>							
-                </ul>
-            </div>			
-        </div>
-    </nav>
+    <?php include 'menu.php';?>
 
     <!--SECTION-->
     <section id="home" class="medium-gray">
---
+++ menu.php
@@ -0,0 +1,46 @@
+    <nav class="navbar navbar-default navbar-fixed-top header-background">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              
+
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#home"><span><img height="700px" src="images/regsol_logo_black_yellow.png"></span></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+					<div class="dropdown nav navbar-left">
+						<li><a href="index.php">Home</a></li>
+					</div>
+					<div class="dropdown nav navbar-left">
+						<li><a href="training.html">Training</a></li>
+					</div>
+						
+					<div class="dropdown nav navbar-left">
+						<li><a href="consulting.php">Consulting</a></li>
+						<div class="dropdown-content">
+							<a href="consulting.php#AML">AML</a>
+							<a href="consulting.php#authorizations">Authorizations</a>		
+							<a href="consulting.php#GDPR">GDPR</a>							
+						</div>
+					</div>
+							
+                    
+					<div class="dropdown nav navbar-left">
+						<li><a href="about_us.html">About Us</a></li>
+						<div class="dropdown-content">
+							<a href="about_us.html#our_story">Our Story</a>
+							<a href="about_us.html#contact">Contact Us</a>		
+							<a href="about_us.html#privacy">Privacy</a>
+						</div>
+					</div>							
+                </ul>
+            </div>			
+        </div>
+    </nav>
