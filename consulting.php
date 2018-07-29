430b9d851c387e55de18b88d7388f2f5a35eb8b1
marksdel <36573913+marksdel@users.noreply.github.com>
2018-07-29 21:39:57 +0100

    Change consulting homepage to list

--- consulting.php
+++ consulting.php
@@ -49,10 +49,58 @@
 			</div>
 			<br><br>
 		</div>		
-		<div class="container centered-column"  >
-			<div class="col-sm-12 col-xs-12 centered-column" >
-			<p>	Our consultancy services cover a wide range of compliance needs and are designed to offer practical Solutions to your Regulatory compliance problems. The experience and expertise of our consultants has allowed us to develop tailored solutions to ensure compliance with Central Bank of Ireland Codes and Regulations, Anti-Money Laundering legislation and Data Protection legislation in particular. We also provide advice on every interaction with regulators and supervisory authorities within those areas including AML Inspections, PRISM Inspections, responding to RMP's, Data Protection Audits, completion of Individual Questionnaires, etc.</p>
+		<div class="container centered-column consulting-body"  >
+			<div class="col-sm-12 col-xs-12 text-left" >
+			Our consultancy services cover a wide range of compliance needs and are designed to offer practical solutions to your regulatory compliance problems. We can also offer assistance on additional areas, please <a href="about.php#contact">contact us</a> with any enquiries.
 			</div>			
+		</div>
+		<div class="container consulting-body">
+			<div class="col-sm-6">
+				<div align="center">
+					<b>Areas of Expertise</b><br><br>
+				</div>
+				<ul>				
+					<li>Central Bank of Ireland Codes and Regulations</li>
+					<li>Anti-Money Laundering Legislation </li>
+					<li>Data Protection Legislation </li>
+					<li>Various Authorisations (e.g. CBI)</li>
+					<li>Interaction with Regulators and Supervisory Authorities</li>
+					<li>Inspections (AML, PRISM, etc)</li>
+					<li>Responding to RMPs</li>
+					<li>Data Protection Audits</li>
+					<li> Data Protection Officer (DPO) Services</li>
+					<li>Completion of Individual Questionnaires</li>
+				</ul>
+			</div>
+			<div class="col-sm-3">
+				<div align="center">
+					<b>Industries Covered</b><br><br>
+				</div>
+				<ul>
+					<li>Insurance</li>
+					<li>Retail</li>
+					<li>Technology</li>
+					<li>Financial Services</li>
+					<li>Construction</li>
+					<li>Education</li>
+					<li>Healthcare</li>
+					<li>Estate Agent</li>
+					<li>Recruitment Agent</li>
+				</ul>				
+			</div>
+			<div class="col-sm-3">
+				<br><br>
+				<ul>
+					<li>Telecommunication</li>
+					<li>Hospitality</li>
+					<li>Manufacturing</li>
+					<li>Food</li>
+					<li>Agricultural</li>
+					<li>Transport</li>
+					<li>Aerospace</li>
+					<li>Pharmaceutical</li>
+				</ul>				
+			</div>
 		</div>
 		        
 		<div class="container">
@@ -144,7 +192,7 @@
 		</div>				
 		<div class="container centered-column">		 
 			<div class="col-sm-12 col-xs-12">
-				<h2> Try our <a href="gdpr-questionnaire.php" target="_new">GDPR Questionnaire</a> to assess your readiness</a>
+				<h2> **Try our <a href="gdpr-questionnaire.php" target="_new">GDPR Questionnaire</a> to assess your readiness**</a>
 				<h2>What is the GDPR?</h2> 
 				<p class="text-left">The GDPR is the <a href="https://www.eugdpr.org/">European Union&rsquo;s General Data Protection Regulation</a>. Its purpose is to &ldquo;harmonize data privacy laws across Europe, to protect and empower all EU residents&rsquo; data privacy, and to reshape the way organizations across the region approach data privacy for EU residents wherever they work in the world.&rdquo;</p> 
 				
